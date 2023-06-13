4ac8a96825e5c46c96664dcfc4719ef74dd3cad1
nobodysGitHub <91459536+nobodysGitHub@users.noreply.github.com>
2023-06-13 15:27:52 +0700

    clean up

--- src/index.ts
+++ src/index.ts
@@ -1,23 +1,7 @@
-import { engine, executeTask, Material, Transform } from '@dcl/sdk/ecs'
-import { Color4 } from '@dcl/sdk/math'
-
-import { createCube } from './factory'
-import { bounceScalingSystem, circularSystem, spawnerSystem } from './systems'
-
-import { setupUi } from './ui'
+import { Transform } from '@dcl/sdk/ecs'
 import { skyboxPZ } from './skybox'
 
 
-
-
-
 export function main() {
-  setupUi()
-
-  // Create my main cube and color it.
-  const cube = createCube(8, 1, 8)
-  Material.setPbrMaterial(cube, { albedoColor: Color4.create(1.0, 0.85, 0.42) })
-
-  
   Transform.getMutable(skyboxPZ)
 }

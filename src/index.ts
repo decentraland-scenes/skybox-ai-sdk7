55bb848e6ab0a7ddcbfbbf4df12a0c5dc3488b99
nobodysGitHub <91459536+nobodysGitHub@users.noreply.github.com>
2023-06-13 16:08:12 +0700

    other skyboxes

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,12 @@
 import { Transform } from '@dcl/sdk/ecs'
 import { skyboxPZ } from './skybox'
+import { movePlayerTo } from '~system/RestrictedActions'
+import { Vector3 } from '@dcl/sdk/math'
+import { height, sceneSizeX, sceneSizeZ } from './resources'
 
 
 export function main() {
   Transform.getMutable(skyboxPZ)
-}
+} 
+
+movePlayerTo({newRelativePosition: Vector3.create(sceneSizeX/2,height/2 + 5,sceneSizeZ/2)})
--- src/resources.ts
+++ src/resources.ts
@@ -1,4 +1,10 @@
 export const sceneSizeX = 6*16
 export const sceneSizeZ = 6*16
 export const height = 6*16
-export const radiusMultiplier = 2
+export const radiusMultiplier = 0.4
+
+/*
+export const sceneSizeX = 6*16
+export const sceneSizeZ = 6*16
+export const height = 6*16
+export const radiusMultiplier = 2*/
